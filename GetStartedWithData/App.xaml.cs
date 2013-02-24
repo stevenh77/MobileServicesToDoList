97ab31b9baa1102382c85c6307dbd29e72e7a9e9
Steven Hollidge <stevenhollidge@hotmail.com>
2013-02-24 10:23:45 +0100

    initial commit

---
+++ GetStartedWithData/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GetStartedWithData
+{
+    sealed partial class App : Application
+    {
+        public static MobileServiceClient MobileService = new MobileServiceClient(
+            "https://todolistdemo1.azure-mobile.net/",
+            "bSirCuVcPKcHWCQVwiXWYLQPeMQexj79"
+        );
+
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            // Do not repeat app initialization when already running, just ensure that the window is active
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+            {
+                Window.Current.Activate();
+                return;
+            }
+
+            if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            {
+                //TODO: Load state from previously suspended application
+            }
+
+            // Create a Frame to act navigation context and navigate to the first page
+            var rootFrame = new Frame();
+            if (!rootFrame.Navigate(typeof(MainPage)))
+            {
+                throw new Exception("Failed to create initial page");
+            }
+
+            // Place the frame in the current Window and ensure that it is active
+            Window.Current.Content = rootFrame;
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ GetStartedWithData/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Runtime.Serialization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace GetStartedWithData
+{    
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [DataMember(Name = "text")]
+        public string Text { get; set; }
+
+        [DataMember(Name = "complete")]
+        public bool Complete { get; set; }
+
+        //[DataMember(Name = "createdAt")]
+        //public DateTime? CreatedAt { get; set; }
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        private MobileServiceCollectionView<TodoItem> items;
+        private IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void InsertTodoItem(TodoItem todoItem)
+        {
+            await todoTable.InsertAsync(todoItem);
+        }
+
+        private void RefreshTodoItems()
+        {
+            //// TODO #1: Defines a simple query for all items. 
+            items = todoTable.ToCollectionView();
+
+            //// TODO #2: More advanced query that filters out completed items. 
+            //items = todoTable
+            //   .Where(todoItem => todoItem.Complete == false)
+            //   .ToCollectionView();
+           
+            ListItems.ItemsSource = items;
+        }
+
+        private async void UpdateCheckedTodoItem(TodoItem item)
+        {
+             await todoTable.UpdateAsync(item);     
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new TodoItem { Text = TextInput.Text };
+            InsertTodoItem(todoItem);
+        }
+
+        private void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            UpdateCheckedTodoItem(item);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+    }
+}
